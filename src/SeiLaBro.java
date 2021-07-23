5509de9290a1208284c500b89a83f7e802ed31ca
felps_mateos <felmateos@gmail.com>
2021-07-23 14:28:57 -0300

    joe mama

--- src/SeiLaBro.java
+++ src/SeiLaBro.java
@@ -1,5 +1,5 @@
 public class SeiLaBro {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         System.out.println("joe");
         System.out.println("joe mama");
 
